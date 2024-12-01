639c0b87a94ce0cecd2e95371e47ad04d3404aad
seven <devmagento2018@gmail.com>
2024-12-01 23:22:14 +0700

    update

--- assets/log-out-Ckfukgub.js
+++
@@ -1,1 +0,0 @@
-import{l as a,i as e,r,m as u,j as t}from"./index-Bn_bHuoN.js";const c=()=>{const{logout:s}=a(),o=e();return r.useEffect(()=>{s(),o(u.HOMEPAGE)}),t.jsx(t.Fragment,{children:"...logout"})};export{c as default};
