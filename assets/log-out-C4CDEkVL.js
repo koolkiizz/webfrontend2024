21676d85aea6a6d7954608761968b0bcb6718ddd
seven <devmagento2018@gmail.com>
2025-03-17 10:46:45 +0700

    update link android ios

---
+++ assets/log-out-C4CDEkVL.js
@@ -0,0 +1,1 @@
+import{l as a,i as e,r,m as u,j as t}from"./index-Eam9zKki.js";const c=()=>{const{logout:s}=a(),o=e();return r.useEffect(()=>{s(),o(u.HOMEPAGE)}),t.jsx(t.Fragment,{children:"...logout"})};export{c as default};
